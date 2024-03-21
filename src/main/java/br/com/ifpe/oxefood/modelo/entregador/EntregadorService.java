0fe02ce7a8856e5d8224a3c774e2d31aae2145b9
aluno <aluno@ifpe>
2024-03-21 16:47:26 -0300

    listagem das classes

--- src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
@@ -1,6 +1,7 @@
 package br.com.ifpe.oxefood.modelo.entregador;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,7 @@ import jakarta.transaction.Transactional;
 public class EntregadorService {
     @Autowired
     private EntregadorRepository repository;
+
     @Transactional
     public Entregador save(Entregador entregador) {
 
@@ -19,4 +21,15 @@ public class EntregadorService {
         entregador.setDataCriacao(LocalDate.now());
         return repository.save(entregador);
     }
+
+    public List<Entregador> listarTodos() {
+
+        return repository.findAll();
+    }
+
+    public Entregador obterPorID(Long id) {
+
+        return repository.findById(id).get();
+    }
+
 }
--- src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
@@ -1,6 +1,7 @@
 package br.com.ifpe.oxefood.modelo.produto;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,9 +16,20 @@ public class ProdutoService {
    @Transactional
    public Produto save(Produto produto) {
 
-        produto.setHabilitado(Boolean.TRUE);
-        produto.setVersao(1L);
-        produto.setDataCriacao(LocalDate.now());
-        return repository.save(produto);
+      produto.setHabilitado(Boolean.TRUE);
+      produto.setVersao(1L);
+      produto.setDataCriacao(LocalDate.now());
+      return repository.save(produto);
    }
+
+   public List<Produto> listarTodos() {
+
+      return repository.findAll();
+   }
+
+   public Produto obterPorID(Long id) {
+
+      return repository.findById(id).get();
+   }
+
 }
